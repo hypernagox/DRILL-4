49bbd742a5d4c655e338688f797e3e80f469adb4
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-28 21:58:29 +0900

    Drill 4 제출 (2022180041 최재혁)
    Add Attack Anim (SPACE KEY)

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -21,16 +21,37 @@ def jumpLeft_anim(frame):
         frame = 0
     megaman.clip_composite_draw(frame * 50, 860 - 50 * 9, 50, 49,0,'h', megaPos[0], megaPos[1],100,100)
 def idleRight_anim(frame):
-    megaman.clip_composite_draw(frame * 50, 860 - 50 * 2, 50, 49, 0, 'h', megaPos[0], megaPos[1], 100, 100)
+    megaman.clip_composite_draw(frame * 50, 860 - 50 * 2, 50, 49, 0, '', megaPos[0], megaPos[1], 100, 100)
 def idleLeft_anim(frame):
-    megaman.clip_composite_draw(frame * 50, 860 - 50 * 2, 50, 49,0,'', megaPos[0], megaPos[1],100,100)
+    megaman.clip_composite_draw(frame * 50, 860 - 50 * 2, 50, 49,0,'h', megaPos[0], megaPos[1],100,100)
 def move_down(frame):
     megaman.clip_composite_draw(frame * 50, 0, 50, 80,0,'', megaPos[0], megaPos[1],100,100)
+
+accTime_atk = 2.
+def attack_left(frame):
+    global accTime_atk
+    global megaman_state
+    accTime_atk -= 0.2
+    if accTime_atk <= 0.:
+        accTime_atk = 2.
+        megaman_state = 0
+        return
+    megaman.clip_composite_draw(frame * 50, 860 - 50 * 3, 50, 49, 0, 'h', megaPos[0], megaPos[1], 100, 100)
+def attack_right(frame):
+    global accTime_atk
+    global megaman_state
+    accTime_atk -= 0.2
+    if accTime_atk <= 0.:
+        accTime_atk = 2.
+        megaman_state = 0
+        return
+    megaman.clip_composite_draw(frame * 50, 860 - 50 * 3, 50, 49, 0, '', megaPos[0], megaPos[1], 100, 100)
 megaman_state = 0
-megaman_state_prev = 0
 def input_handler():
     global dx,dy
     global prevX,prevY
+    global running
+    global megaman_state
     events = get_events()
     for eve in events:
         if eve.type == SDL_KEYDOWN:
@@ -42,6 +63,13 @@ def input_handler():
                 dy += 1
             elif eve.key == SDLK_DOWN:
                 dy -= 1
+            elif eve.key == SDLK_SPACE:
+                if prevX == -1:
+                    megaman_state = 7
+                else :
+                    megaman_state = 8
+            elif eve.key == SDLK_ESCAPE:
+                running = False
         elif eve.type == SDL_KEYUP:
             if eve.key == SDLK_RIGHT:
                 prevX = dx
@@ -55,6 +83,8 @@ def input_handler():
                 dy += 1
 def update_state():
     global  megaman_state
+    if megaman_state == 7 or megaman_state == 8:
+        return
     if dy == 1:
         if prevX == 1:
             megaman_state = 4
@@ -75,23 +105,26 @@ def IsCollision(pos,size):
     if pos[0] >= 0 + size and pos[0] <= 800 - size and pos[1] >= 0 + size and pos[1] <=600 -size:
         return False
     return True
-commands = {0:idleLeft_anim,1:idleRight_anim,2:runLeft_anim,3:runRight_anim,4:jumpRight_anim,5:jumpLeft_anim,6:move_down}
+commands = {0:idleRight_anim,1:idleLeft_anim,2:runLeft_anim,3:runRight_anim,4:jumpRight_anim,5:jumpLeft_anim,6:move_down,7:attack_left,8:attack_right}
 dx,dy=0,0
 speed = 10
 prevX,prevY = -1,-1
+running = True
 while True:
     input_handler()
+    if not running:
+        break
     update_state()
     megaPos[0] += dx * speed
     megaPos[1] += dy * speed
     if IsCollision(megaPos,50):
         megaPos[0] -= dx * speed
         megaPos[1] -= dy * speed
-    backGround.draw(TUK_WIDTH//4,TUK_HEIGHT//4)
+    backGround.composite_draw(0,'',400,300,800,600)
     commands[megaman_state](curFrame)
 
     update_canvas()
     clear_canvas()
     curFrame = (curFrame + 1) % 8
     delay(0.02)
-
+close_canvas()
